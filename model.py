1a743e8f0dfaaa6d4c28938dfc9c0b95174ff82a
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-22 21:21:40 +0100

    plotted town data

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
 import townframework
+import matplotlib.pyplot
 
-class Location:
-    def __init__(self, address):
-        self.address = address
   
 town = townframework.load('drunk.txt')
+plotTown = []
+for row in town:
+    plotRow = []
+    plotTown.append(plotRow)
+    for location in row:
+        plotRow.append(location.address)
+
+
+matplotlib.pyplot.imshow(plotTown)
+
+matplotlib.pyplot.show()
