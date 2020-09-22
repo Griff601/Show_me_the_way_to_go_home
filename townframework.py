57e3edf88bc1dbcb6ee17d00493cda06f531de6c
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-22 21:08:01 +0100

    initialising a town with a location

--- townframework.py
+++ townframework.py
@@ -1,5 +1,9 @@
 import csv
 
+class Location:
+    def __init__(self, address):
+        self.address = address
+
 def load(file): 
     with open(file, newline='') as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
@@ -8,7 +12,7 @@ def load(file):
         for row in reader:
             rowlist = []
             for value in row:
-                rowlist.append(value)
+                rowlist.append(Location(value))
             
             town.append(rowlist)
     return town
