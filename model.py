cf192021bad1156db5e3a1455c519aa328557078
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-23 22:59:51 +0100

    working on density of drunks passing each point

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 import townframework
 import matplotlib.pyplot
 
-iterations = 10000
+iterations = 1000
 
 town = townframework.load('drunk.txt')
+
 
 for i in range (iterations):
     for drunk in town.drunks:
         drunk.wander()
+
+# plotting the town and drunks as they initialise and move
 
 matplotlib.pyplot.imshow(town.environment)
 for drunk in town.drunks:
--- townframework.py
+++ townframework.py
@@ -15,26 +15,45 @@ class Town:
         pub = locations[1]
         possibleStarts = pub.points.copy()
         # create drunk for each location that is not the pub
-        for location in locations:
+        for key, location in locations.items():
             if location == pub:
                 continue
             # selecting start point within pub
             x, y = random.choice(possibleStarts)
             possibleStarts.remove((x, y))
-            self.drunks.append(Drunk(x, y, location))
+            # create drunk
+            self.drunks.append(Drunk(x, y, location, environment))
         
 
 class Drunk:
-    def __init__(self, x, y, home):
+    def __init__(self, x, y, home, environment):
         self.home = home
         self.x = x
         self.y = y
+        self.environment = environment
     # setting up the wandering of drunks
     def wander(self):
+        # stopping the drunks from wandering when they reach their home
+        imHome = (self.x, self.y) in self.home.points
+        if imHome:
+            return
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         x, y = random.choice(directions)
+        # these checks will stop the drunks leaving the boundary of the town
+        # but drunks will get stuck on the outer edge and 1 in 3 times will
+        # not be able to move away. 
+        nextX = self.x + x
+        nextY = self.y + y
+        if nextY < 0 or nextY >= len(self.environment):
+            return
+        if nextX < 0 or nextX >= len(self.environment[nextY]):
+            return
         self.x += x
         self.y += y
+        # changing the environment to show individual cells that have been 
+        # passed
+        #self.environment[y][x] += 1
+        
         
 
 def load(file): 
