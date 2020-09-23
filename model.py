4080a4c14136b38c0ec561ba559766a8f7b4396a
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-23 20:34:05 +0100

    set up class for town and building environment within load

--- model.py
+++ model.py
@@ -3,14 +3,8 @@ import matplotlib.pyplot
 
   
 town = townframework.load('drunk.txt')
-plotTown = []
-for row in town:
-    plotRow = []
-    plotTown.append(plotRow)
-    for location in row:
-        plotRow.append(location.address)
 
 
-matplotlib.pyplot.imshow(plotTown)
+matplotlib.pyplot.imshow(town.environment)
 
 matplotlib.pyplot.show()
--- townframework.py
+++ townframework.py
@@ -4,15 +4,36 @@ class Location:
     def __init__(self, address):
         self.address = address
 
+class Town:
+    def __init__(self, locations, environment):
+        self.locations = locations
+        self.drunks = []
+        self.environment = environment
+
+class Drunk:
+    def __init__(self, x, y, home):
+        self.home = home
+        self.x = x
+        self.y = y
+
 def load(file): 
+    locations = {}
+    environment = []
+    
     with open(file, newline='') as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    
-        town = []
+            
         for row in reader:
             rowlist = []
             for value in row:
-                rowlist.append(Location(value))
-            
-            town.append(rowlist)
-    return town
+                rowlist.append(value)
+                if value == '0':
+                    continue
+                if not value in locations:
+                    locations[value] = Location(value)
+                # asign points
+            environment.append(rowlist)
+    return Town(locations, environment)
+
+
+        
