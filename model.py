ebea89595f3f37fe32bf00abc5e3597521f979d5
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-23 21:52:00 +0100

    created a wander function and trialed it successfuly. number of iterations tested at 100, 1000 and 10,000

--- model.py
+++ model.py
@@ -1,10 +1,15 @@
 import townframework
 import matplotlib.pyplot
 
-  
+iterations = 10000
+
 town = townframework.load('drunk.txt')
 
+for i in range (iterations):
+    for drunk in town.drunks:
+        drunk.wander()
 
 matplotlib.pyplot.imshow(town.environment)
-
+for drunk in town.drunks:
+    matplotlib.pyplot.scatter(drunk.x, drunk.y)
 matplotlib.pyplot.show()
--- townframework.py
+++ townframework.py
@@ -4,6 +4,7 @@ import random
 class Location:
     def __init__(self, address):
         self.address = address
+        self.points = []
 
 class Town:
     def __init__(self, locations, environment):
@@ -28,6 +29,13 @@ class Drunk:
         self.home = home
         self.x = x
         self.y = y
+    # setting up the wandering of drunks
+    def wander(self):
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        x, y = random.choice(directions)
+        self.x += x
+        self.y += y
+        
 
 def load(file): 
     locations = {}
@@ -35,17 +43,26 @@ def load(file):
     
     with open(file, newline='') as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-            
+        y = 0
+             
         for row in reader:
             rowlist = []
+            x = 0
             for value in row:
                 rowlist.append(value)
                 if value == '0':
                     continue
                 if not value in locations:
                     locations[value] = Location(value)
-                # asign points
+                    
+                # asign points to locations
+                l = locations[value]
+                p = l.points
+                p.append((x, y))
+                x += 1
             environment.append(rowlist)
+            y += 1
+            
     return Town(locations, environment)
 
 
