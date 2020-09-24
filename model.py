65a80e0b2bb1fbefaa5fa6bd221d723d01db3082
Griff601 <62428806+Griff601@users.noreply.github.com>
2020-09-24 22:46:29 +0100

    added a write to file code, blended heatmap with town map
    heat map results was blended with the map map of the town (environment) in order to show the density tracks of the drunks movement with the resulting locations of the drunks

--- model.py
+++ model.py
@@ -1,20 +1,42 @@
-#
+# importing modules required
 import townframework
 import matplotlib.pyplot
+import csv
 
-#
+# trial and error used to find a 'best fit' result. 100 resulted in little 
+# movement of drunks, 500 was good, but 1000 iterations put most drunks at home
 iterations = 1000
 
+# loading txt file into townframework.py 
 town = townframework.load('drunk.txt')
 
-# rendering the town environment
-matplotlib.pyplot.imshow(town.environment)
 
 # run the model
 for i in range (iterations):
     town.think()
 
+# rendering the town environment
+matplotlib.pyplot.imshow(town.environment)
+
 # plotting the end point of the drunks as they initialise and move
 for drunk in town.drunks:
     matplotlib.pyplot.scatter(drunk.x, drunk.y)
+# matplotlib.pyplot.show()  # un-comment to view tracks on seperate plot
+
+# rendering heat map of points used by drunks
+# apha blend used to allow tracks to be seen on one plot with drunks and houses
+matplotlib.pyplot.imshow(town.heatMap, alpha = 0.5)
 matplotlib.pyplot.show()
+
+for drunk in town.drunks:
+    if drunk.imHome:
+        message = "got home safely"
+    else:
+        message = "is still at large"
+    print("The drunk for house", drunk.home.address, message)
+    
+# writing results to CSV file
+with open('results.txt', 'w', newline='') as output:
+     wr = csv.writer(output, quoting=csv.QUOTE_ALL)
+     for row in town.heatMap:
+         wr.writerow(row)
--- townframework.py
+++ townframework.py
@@ -1,17 +1,24 @@
+# imorting required modules for code
 import csv
 import random
 import math
 
+# creating locations class and initialising it with: self and address and 
+# creating points list. 
 class Location:
     def __init__(self, address):
         self.address = address
         self.points = []
 
+# creating town class where the 'map' is initialised 
 class Town:
     def __init__(self, locations, environment):
         self.locations = locations
         self.drunks = []
         self.environment = environment
+        self.heatMap = []
+        for row in environment:
+            self.heatMap.append([0] * len(row))
         # locate pub values
         pub = locations[1]
         possibleStarts = pub.points.copy()
@@ -23,7 +30,7 @@ class Town:
             x, y = random.choice(possibleStarts)
             possibleStarts.remove((x, y))
             # create drunk
-            self.drunks.append(Drunk(x, y, location, environment, self.drunks, pub))
+            self.drunks.append(Drunk(x, y, location, self, pub))
     def think(self):
         # randomising the drunks on every iteration in order to remove 
         # artifacts
@@ -32,18 +39,20 @@ class Town:
         
 
 class Drunk:
-    def __init__(self, x, y, home, environment, drunks, pub):
+    def __init__(self, x, y, home, town, pub):
         self.home = home
         self.x = x
         self.y = y
-        self.environment = environment
-        self.drunks = drunks
+        self.environment = town.environment
+        self.drunks = town.drunks
         self.pub = pub
+        self.heatMap = town.heatMap
+        self.imHome = False
     # setting up the wandering of drunks
     def wander(self):
         # stopping the drunks from wandering when they reach their home
-        imHome = (self.x, self.y) in self.home.points
-        if imHome:
+        self.imHome = (self.x, self.y) in self.home.points
+        if self.imHome:
             return
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         directions.append(self.nextDirectionHome(directions))
@@ -60,7 +69,8 @@ class Drunk:
                 self.y += y
                 # changing the environment to show individual cells that have been 
                 # passed
-                self.environment[y][x] += 1
+                
+                self.heatMap[self.y][self.x] += 1
                 break
     def distanceBetween(self, p1, p2):
         return abs (math.sqrt(((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2)))
